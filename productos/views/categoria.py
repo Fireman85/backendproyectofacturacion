39ac422988f7ad1841b0166204eaf82626f78f5e
Fireman Pineda Orozco <ing.firemann@gmail.com>
2025-04-10 21:06:28 -0500

    Metodo list explicito en view Categoria

--- productos/views/categoria.py
+++ productos/views/categoria.py
@@ -1,6 +1,6 @@
 
 from rest_framework import viewsets
-
+from rest_framework.response import Response
 # mi modelo creado
 from productos.models import Categoria
 # mi serializer creado
@@ -10,3 +10,14 @@ from productos.serializers import CategoriaSerializer
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
