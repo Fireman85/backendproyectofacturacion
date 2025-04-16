a4fe8145cf53ec900b6de54ee79d1bb583656442
Fireman Pineda Orozco <ing.firemann@gmail.com>
2025-04-15 21:09:44 -0500

    Ajuste en serializer productos, para que al obtener los productos se obtenga tambien el nombre de la categoria asociada a ese producto

--- productos/serializers/producto.py
+++ productos/serializers/producto.py
@@ -3,6 +3,9 @@ from productos.models import Producto
 
 
 class ProductoSerializer(serializers.ModelSerializer):
+    nombre_categoria = serializers.StringRelatedField(source='categoria',
+                                                      read_only=True)
+
     class Meta:
         model = Producto
         fields = '__all__'
